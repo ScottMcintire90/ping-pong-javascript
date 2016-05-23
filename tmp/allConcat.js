1dddef03a2ebf585b408499f81ff3adc76b13be3
scott mcintire <scottmcintire90@gmail.com>
2016-05-23 09:54:30 -0700

    finished hw

---
+++ tmp/allConcat.js
@@ -0,0 +1,37 @@
+function pingPong(goal) {
+  var output = [];
+  for (var i = 1; i <= goal; i++) {
+    if (i % 15 == 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else  {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+var pingPong = require('./../js/ping-pong.js').pingPong;
+
+$(document).ready(function(){
+  $('#ping-pong').submit(function(event){
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element){
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
