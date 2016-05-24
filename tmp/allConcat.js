2ee96308a63d1dcbfb9445257aa67862c9a67ebb
scott mcintire <scottmcintire90@gmail.com>
2016-05-24 09:12:02 -0700

    undelete everything

---
+++ tmp/allConcat.js
@@ -0,0 +1,51 @@
+function pingPong(goal) {
+  var output = [];
+  for (var i = 1; i <= goal; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else  {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+var pingPong = require('./../js/ping-pong.js').pingPong;
+
+$(document).ready(function(){
+  $('#ping-pong').submit(function(event){
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element){
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
+
+var apiKey = "9b725badda2db5e91674eb1c72cd1aeb";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+  });
+});
